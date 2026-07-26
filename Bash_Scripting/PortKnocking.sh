1556384928b05785f3c025972f8d9afdfba41bc9
BR45H <275584516+BR45H@users.noreply.github.com>
2026-07-26 03:46:50 -0300

    fix: bugs

--- Bash_Scripting/PortKnocking.sh
+++ Bash_Scripting/PortKnocking.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-if [ -z "$1" ]
+if [[ -z "$1" || -z "$2" ]]
 then 
     echo "PortKnocking Bash Script"
-    echo "Usage: $0 <IP/Domain>"
+    echo "Usage: $0 <IP/Domain> <Port_dst>"
     exit 1
 fi
+
+target="$1"
+target_port="$2"
 
 read -p "Insert the port sequence: " port_sequence
 
 for port in $port_sequence; do
-    nc -w1 -z -v "$1" "$port"
+    nc -w1 -z "$target" "$port" 2>/dev/null
 done
 
-if [ $? -eq 0 ]; then
-    echo "Port knocking sucessful!"
+if nc -w2 -z "$target" "$target_port" 2>/dev/null; then
+    echo "Port knocking sucessful, port $target_port is open."
 else
-    echo "Port knocking failed."
+    echo "Port knocking failed, port $target_port remains closed."
 fi
