22fa876b932440fb1cbb255fb20bfcdfaca78e27
BR45H <275584516+BR45H@users.noreply.github.com>
2026-07-26 20:09:58 -0300

    fix: packet duplication

--- Bash_Scripting/PortKnocking.sh
+++ Bash_Scripting/PortKnocking.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 if [[ -z "$1" || -z "$2" ]]
 then 
-    echo "PortKnocking Bash Script"
-    echo "Usage: $0 <IP/Domain> <Port_dst>"
-    exit 1
+	echo "PortKnocking Bash Script"
+    	echo "Usage: $0 <IP/Domain> <Port_dst>"
+    	exit
+fi
+
+if [ "$EUID" -ne 0 ]
+then
+	echo "Run the script as a root."
+	exit
 fi
 
 target="$1"
@@ -12,11 +18,14 @@ target_port="$2"
 read -p "Insert the port sequence: " port_sequence
 
 for port in $port_sequence; do
-    nc -w1 -z "$target" "$port" 2>/dev/null
+    hping3 -S -c 1 -p "$port" "$target" >/dev/null 2>&1
 done
 
-if nc -w2 -z "$target" "$target_port" 2>/dev/null; then
-    echo "Port knocking sucessful, port $target_port is open."
+if hping3 -S -c 1 -p "$target_port" "$target" >/dev/null 2>&1; then
+	echo "Port knocking sucessful, port $target_port is open."
+	echo "Grabbing banner..."
+	echo "" | timeout 3 nc "$target" "$target_port"
+
 else
-    echo "Port knocking failed, port $target_port remains closed."
+	echo "Port knocking failed, port $target_port remains closed."
 fi
