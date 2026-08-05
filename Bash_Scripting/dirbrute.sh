1558b66e705ad65d9531986abec19776addce7db
BR45H <275584516+BR45H@users.noreply.github.com>
2026-08-04 22:22:09 -0300

    fix: bugs

--- Bash_Scripting/dirbrute.sh
+++ Bash_Scripting/dirbrute.sh
@@ -19,11 +19,11 @@ while IFS= read -r path; do
     [[ -z "$path" ]] && continue
 
     status=$(curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0" \
-        -s -o /dev/null -w "%{http_code}" "${target}/${path}" < /dev/null)
+        -s -o /dev/null -w "%{http_code}" "${target}/${path}")
     
     case "$status" in 
         200|301|302|403)
-            echo "[$status] ${url}/${caminho}"
+            echo "[$status] ${target}/${path}"
             ;;
     esac
 done < "$wordlist"
